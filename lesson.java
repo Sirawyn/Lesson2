148e89e86bebebca8501f2df0d34eae26c17f4aa
Sirawyn <devindeng6793@gmail.com>
2019-03-10 12:12:53 -0400

    idk what to write here

---
+++ lesson.java
@@ -0,0 +1,24 @@
+/* hellothere variables. give intgers calue, giveint5; to declare variable: intger, boolean, float, double, boolean flag=false  two loops: For Loop, While Loop*/
+public class lesson {
+  public static void main (String[] args){
+    System.out.println("hello");
+    int a=5;
+    System.out.println(a);
+    for(int i=0;i<10;i++){
+      System.out.println("hello there.generalkenobi");
+    }
+    int y=0;
+    while(y<=10){
+    System.out.println("test2");
+    y++;
+    }
+    for(int x=10;x<=20;x+=2){
+    System.out.println(x);
+    }
+    int o=10;
+    while(o<=20;x+2){
+    System.out.println
+
+    }
+  }
+}
