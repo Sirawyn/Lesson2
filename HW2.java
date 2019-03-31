8abc0767e1a522e9809cb07949eac49263e5156f
Sirawyn <48410454+Sirawyn@users.noreply.github.com>
2019-03-30 20:44:22 -0400

    Add files via upload

---
+++ HW2.java
@@ -0,0 +1,62 @@
+public class Homework2/Q1{
+  public static void main(String[] args){
+    for(int x = 1; x <=60; x+=1){
+      int x = F
+        System.out.println(x);
+    }
+    for(int y = 61; y <=70; y+1){
+      int y = D
+        System.out.println(y);
+    }
+    for(int z = 71; z <=80; z+1){
+      int z = C
+        System.out.println(z);
+    }
+    for(int u = 81; u <=90; u+1){
+      int z = B
+        System.out.println(u);
+    }
+    for(int n = 81; n <=90; n+1){
+      int n = B
+        System.out.println(n);
+    }
+  }
+}
+
+public class Homework2/Q2{
+  public static void main(String[] args){
+    for(int x = 3; x+=3){
+      System.out.println(x);
+    }
+      for(int y = 4; y+=4){
+      System.out.println(y);
+    }
+  }
+}
+
+public class Homework/Q3{
+  public static void main(String[] args){
+    int a = 1
+    int a = 2
+    int a = 3
+    System.out.println(3)
+  }
+}
+
+public class Homework2/Q4{
+  public static void main(String[] args){
+    System.out.println("idkman");
+    jframe.window = null
+  }
+}
+
+public class Homework/Q5{
+  public static void main(String[] args){
+    int a = 1
+    int b = 2
+    int c = 3
+    int d = 4
+    int e = 5
+    System.out.println(c);
+  }
+}
